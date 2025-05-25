055055f80c4bfcccc5ec5280ff3a86415c53f1b8
Monsieur KOLUJ <misterfrpkoluj@gmail.com>
2025-05-25 16:01:48 +0200

    Update Inscription.js

--- models/Inscription.js
+++ models/Inscription.js
@@ -2,11 +2,10 @@ const mongoose = require('mongoose');
 
 const InscriptionSchema = new mongoose.Schema({
     nom: { type: String, required: true },
-    telephone: { type: String, required: true },
-    codePays: { type: String, required: true },
+    phone : { type: String, required: true },
     email: { type: String, required: true },
     role: { type: String, required: true },
-    message: { type: String, required: true }
+    motivation : { type: String, required: false }
 });
 
 const Inscription = mongoose.model('Inscription', InscriptionSchema);
